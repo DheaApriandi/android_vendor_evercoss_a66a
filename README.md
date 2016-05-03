88f802ac0eb750d8096924658e8118e834fe824e
dheaapriandi <dheaapriandi@gmail.com>
2016-05-03 12:22:57 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Vendor blobs for Evercoss A66A
+---------------
+
+* Check device tree here:
+https://github.com/dheaapriandi/android_device_evercoss_a66a.git
+
+* Compilation
+
+        # repo init -u git://github.com/dheaapriandi/android.git -b cm-13.0
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_a66a-userdebug
+
+---------------
---
+++ a66a-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/xbin,system/xbin)
+
